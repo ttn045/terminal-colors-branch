ca60bc48b608145f2aebf1041122a390c18171ea
ttn045 <salhi.mootezbelleh@gmail.com>
2024-10-23 04:26:52 +0200

    terminal-colors-branch : A Script Has Been Added
    This script allows you to create a visually appealing terminal that
    displays the name of the current branch in color. This feature is
    especially useful when working with Git.
    
    
    Signed-off-by: ttn045 <salhi.mootezbelleh@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# terminal-colors-branch
+# Give Your Terminal a Professional Touch!
+
+1. **Open your `.bashrc` file:**
+   ```bash
+   nano ~/.bashrc
+
+2. **Add your content**
+
+3. **Save and exit**
+
+4. **Apply the changes:**
+   ```bash
+   source ~/.bashrc
---
+++ terminal-colors-branch.sh
@@ -0,0 +1,45 @@
+# -------------- terminal-colors-branch ----------------
+
+# Colors for the prompt
+blue="\033[01;34m"
+white="\033[01;37m"
+green="\033[01;32m"
+yellow="\033[0;33m"
+pink="\033[1;35m"
+
+# Brackets needed around non-printable characters in PS1
+ps1_blue='\['"$blue"'\]'
+ps1_green='\['"$green"'\]'
+ps1_white='\['"$white"'\]'
+ps1_yellow='\['"$yellow"'\]'
+ps1_pink='\['"$pink"'\]'
+
+parse_git_branch() {
+    gitstatus=`git status 2> /dev/null`
+    if [[ `echo $gitstatus | grep "Changes to be committed"` != "" ]]
+    then
+        git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1***)/'
+    elif [[ `echo $gitstatus | grep "Changes not staged for commit"` != "" ]]
+    then
+        git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1**)/'
+    elif [[ `echo $gitstatus | grep "Untracked"` != "" ]]
+    then
+        git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1*)/'
+    elif [[ `echo $gitstatus | grep "nothing to commit"` != "" ]]
+    then
+        git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+    else
+        git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1?)/'
+    fi
+}
+
+parse_git_branch_color() {
+    br=$(parse_git_branch)
+    if [[ $br == "(master)" || $br == "(master*)" || $br == "(master**)" || $br == "(master***)" ]]; then
+        echo -e "${blue}"
+    else
+        echo -e "${yellow}"
+    fi
+}
+
+export PS1="$ps1_green@\h:$ps1_pink\W\[ \$(parse_git_branch_color)\]\$(parse_git_branch) $ps1_blue\$$ps1_white "
